80200ebc2ca9df3849843973ad6aa0100f974b19
dupborges <dupborges@gmail.com>
2022-06-04 10:27:42 -0300

    apaga tudo

--- start_report_.bat
+++
@@ -1,5 +0,0 @@
-call conda activate env_fermentacao
-
-
-call streamlit run APP_fermentacao_1.0_.py
-call conda deactivate
--- start_report_com_instalacao.bat
+++
@@ -1,6 +0,0 @@
-call conda activate env_fermentacao
-
-call pip install -r "bibliotecas_fermentacao.txt"
-
-call conda deactivate
-
