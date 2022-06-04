2e61dc5157afa836f157e8e1dbe04f3b580d477b
dupborges <dupborges@gmail.com>
2022-06-04 01:22:06 -0300

    ssss

--- start_report_.bat
+++ start_report_.bat
@@ -1,6 +1,5 @@
 call conda activate env_fermentacao
-pip freeze > requirement.txt
-conda env export > environment.yml --from-history
+
 
 call streamlit run APP_fermentacao_1.0_.py
 call conda deactivate
